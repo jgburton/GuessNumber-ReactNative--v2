e933ca043d774905afb92b7dd720a9a044c9c5b2
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-07-21 12:02:58 +0100

    minor imporvment to make code more dynamic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -125,7 +125,7 @@ const GameScreen = props => {
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card style={styles.buttonContainer}>
+      <Card style={[...styles.buttonContainer, {marginTop: availableHeight > 600 ? 20 : 5}]}>
         <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
           <Ionicons name="md-remove" size={24} color="white" />
         </MainButton>
@@ -157,8 +157,6 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    // marginTop: 20,
-    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%'
   },
