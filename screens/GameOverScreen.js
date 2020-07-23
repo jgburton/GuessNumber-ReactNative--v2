3039e507a92d3821be44ce5459b48158ce9a0e89
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-07-23 16:05:44 +0100

    added SafeAreaView

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image, Dimensions, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,32 +8,32 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
   return (
-    <ScrollView>
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../assets/success.png')}
-          // source={{
-          //   uri:
-          //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
-          // }}
-          style={styles.image}
-          resizeMode="cover"
-        />
-      </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{' '}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+      <ScrollView>
+        <View style={styles.screen}>
+          <TitleText>The Game is Over!</TitleText>
+          <View style={styles.imageContainer}>
+            <Image
+              source={require('../assets/success.png')}
+              // source={{
+              //   uri:
+              //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
+              // }}
+              style={styles.image}
+              resizeMode="cover"
+            />
+          </View>
+          <View style={styles.resultContainer}>
+            <BodyText style={styles.resultText}>
+              Your phone needed{' '}
+              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
           guess the number{' '}
-          <Text style={styles.highlight}>{props.userNumber}</Text>.
+              <Text style={styles.highlight}>{props.userNumber}</Text>.
         </BodyText>
-      </View>
+          </View>
 
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
-    </ScrollView>
+          <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+        </View>
+      </ScrollView>
   );
 };
 
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: Dimensions.get('window').width * 0.7,
     height: Dimensions.get('window').width * 0.7,
-    borderRadius: Dimensions.get('window').width * 0.7/2,
+    borderRadius: Dimensions.get('window').width * 0.7 / 2,
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden',
